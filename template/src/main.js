863cdaf6dc5cb997dc4068d57e25dce7ed32cb27
suhao <valar_su@163.com>
2017-11-23 17:24:48 +0800

    rm vuex

--- template/src/main.js
+++ template/src/main.js
@@ -21,11 +21,6 @@ Vue.use(VueAxios, axios){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 {{/mtue}}
 Vue.config.productionTip = false{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
-{{#vuex}}  //vuex为true的时候就会写入这些
-import Vuex from 'vuex'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-import store from  './store/store'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-Vue.use(Vuex){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-{{/vuex}}
 
 
 /* eslint-disable no-new */
@@ -34,9 +29,6 @@ new Vue({
   {{#router}}
   router,
   {{/router}}
-  {{#vuex}}
-  store,
-  {{/vuex}}
   {{#if_eq build "runtime"}}
   render: h => h(App){{#if_eq lintConfig "airbnb"}},{{/if_eq}}
   {{/if_eq}}
